eb65ea19aea1c04f879f9d7b774bcd20dfccc88c
Edwin <edwin.barrera1025@gmail.com>
2026-04-07 13:48:50 +0200

    Generando carpeta por nivel

---
+++ Tasca S1.02. PHP bàsics/nivel1/nivel1.php
@@ -0,0 +1,13 @@
+<?php
+//Exercici 1
+$string = "Hola soy un texto!";
+$integer = 1;
+$float = 3.1416;
+$boolean = false;
+//$arrayString = array("PHP", "CSS", "HTML", "JS");
+//$arrayIntegers = array(0, 1, 2, 3);
+
+echo "String: $string \n";
+echo "Integer: $integer \n";
+echo "Float: $float \n";
+echo "Boolean: $boolean";
