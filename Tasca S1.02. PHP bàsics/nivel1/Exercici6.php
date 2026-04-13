a3d03645420ef4d52eeb9cce8ed60727edc77595
Edwin <edwin.barrera1025@gmail.com>
2026-04-13 10:59:43 +0200

    Revision 1: Ejercicio 6, agregado tipado especifico

--- Tasca S1.02. PHP bàsics/nivel1/Exercici6.php
+++ Tasca S1.02. PHP bàsics/nivel1/Exercici6.php
@@ -1,12 +1,12 @@
 <?php
-function isBitten()
+function isBitten(): bool
 {
     $bitten = ((float)mt_rand(0, 100)) / 100;
     return ($bitten > 0.5);
 }
 
 do {
-    $bitten = (bool)isBitten();
+    $bitten = isBitten();
     echo "\nDani" . ($bitten ? " " : " NO ") . "te ha mordido el dedo!\n";
     $option = readline("Deseas continuar? (si/no)");
 } while ($option == "si");
